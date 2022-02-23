cece65a7b0b25c0c0b6b0b4209a9e9613a71b48a
Shuvam Sahoo <shuvam.sahoo@yesgnome.com>
2022-02-23 16:52:32 +0530

    Level Manager Logic Done.

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
@@ -9,6 +10,12 @@ public class LevelManager : MonoBehaviour
         if (collision.gameObject.tag == "Player")
         {
             Debug.Log("Level 1 Completed");
+            GotoNextLevel();
         }
+    }
+
+    public void GotoNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
