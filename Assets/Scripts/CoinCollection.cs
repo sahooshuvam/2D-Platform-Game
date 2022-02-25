d631a7d97149c66c9d1f1255e37a1fa7523f6776
Shuvam Sahoo <shuvam.sahoo@yesgnome.com>
2022-02-26 00:22:15 +0530

    On collection  of the coin... coin will destroy...

---
+++ Assets/Scripts/CoinCollection.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCollection : MonoBehaviour
+{
+    ScoreManager score;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+        score = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
+        score.ScoreCalculator(10);
+    }
+}
